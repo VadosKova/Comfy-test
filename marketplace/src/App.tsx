b7a7b48581667a22f0585947503f25d6b01e763f
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-11-27 12:30:43 +0200

    Fixed

--- marketplace/src/App.tsx
+++ marketplace/src/App.tsx
@@ -1,5 +1,13 @@
 import Header from './Header'
 import './App.css'
+import phoneIcon from './assets/phone.png';
+import laptopIcon from './assets/laptop.png';
+import ovenIcon from './assets/oven.png';
+import washmachIcon from './assets/washing-machine.png';
+import tvIcon from './assets/tv.png';
+import smartwatchIcon from './assets/smartwatch.png';
+import leftIcon from './assets/left-arrow.png';
+import rightIcon from './assets/right-arrow.png';
 
 function App() {
 
@@ -9,7 +17,55 @@ function App() {
 
       <Header />
 
+      <div className='section-header'>
+        <div className="header__breadcrumbs">
+          <span className="header__divider">Головна</span>
+          <span className="header__divider">Чорна п'ятниця</span>
+        </div>
+        
+        <img src="https://cdn.comfy.ua/media/x/brending-bannera/1366x200_1072.jpg"></img>
 
+        <div className="categories-wrapper">
+          <button className='not-active-carousel'>
+            <img src={leftIcon} className="icon" />
+          </button>
+          <div className="categories">
+            <div className="categories__title">
+              <div className='categorie-count'>1850</div>
+              <img src={phoneIcon} className="icon" />
+              Смартфони та телефони
+            </div>
+            <div className="categories__title">
+              <div className='categorie-count'>10140</div>
+              <img src={laptopIcon} className="icon" />
+              Ноутбуки, планшети та комп'ютерна техніка
+            </div>
+            <div className="categories__title">
+              <div className='categorie-count'>3877</div>
+              <img src={ovenIcon} className="icon" />
+              Техніка для кухні
+            </div>
+            <div className="categories__title">
+              <div className='categorie-count'>1552</div>
+              <img src={washmachIcon} className="icon" />
+              Техніка для дому
+            </div>
+            <div className="categories__title">
+              <div className='categorie-count'>670</div>
+              <img src={tvIcon} className="icon" />
+              Телевізори та мультимедіа
+            </div>
+            <div className="categories__title">
+              <div className='categorie-count'>364</div>
+              <img src={smartwatchIcon} className="icon" />
+              Смарт-годинники та гаджети
+            </div>
+          </div>
+          <button className='active-carousel'>
+            <img src={rightIcon} className="icon" />
+          </button>
+        </div>
+      </div>
     </>
   )
 }
